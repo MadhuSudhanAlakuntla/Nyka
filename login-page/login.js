57c9b07173ef30238e851052ce7517c8e4b693b0
KGS97 <work.dipanshu.singh@gmail.com>
2023-08-14 15:34:08 +0530

    completed login and signup journey

--- login-page/login.js
+++ login-page/login.js
@@ -6,12 +6,9 @@ var userData4 = JSON.parse(localStorage.getItem("user-data")) || [];
 var form4 = document.querySelector("form");
 form4.addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log(event);
    if(event.target.otpagain.value==(1234)){
-
-    alert("enter home page link");
-    //enter home page link
-  //  window.location.assign("almostthere.html");
+window.location.assign('../index.html')
+    
    }else{
     alert(" Otp verification failed!");
    }
