b9094e41972194ee505178a587b70ffb2319d7dd
KGS97 <work.dipanshu.singh@gmail.com>
2023-08-16 22:52:08 +0530

    Merge branch 'main' of

--- cartAndPayment/cart.js
+++ cartAndPayment/cart.js
@@ -1,24 +1,26 @@
+let products = JSON.parse(localStorage.getItem('cart-products')) || []
 
-let pds = JSON.parse(localStorage.getItem('cart-products'))||[];
-let items = document.getElementById('items')
+var items = document.getElementById('items')
 function display(products){
     items.innerHTML = "";
+<<<<<<< HEAD
     document.getElementById('tp').style.display = 'flex'
+=======
+>>>>>>> cbd2d317f70bf6b965df1f48712720d4789c4ed1
     document.getElementById('total-items').innerText = products.length;
     products.forEach(function(element,index){
-        let item = document.createElement('div'); 
+        var item = document.createElement('div'); 
         item.className = 'item';
-        let div = document.createElement('div'); 
-        let img = document.createElement('img');
-        let name = document.createElement('p');
-        let qty = document.createElement('p');
-        let price = document.createElement('h6');
-        let btn = document.createElement('button');
+        var div = document.createElement('div'); 
+        var img = document.createElement('img');
+        var name = document.createElement('p');
+        var qty = document.createElement('p');
+        var price = document.createElement('h6');
+        var btn = document.createElement('button');
         btn.textContent = 'Remove';
         btn.addEventListener('click',function(event){
             event.preventDefault();
             products.splice(index,1);
-            localStorage.setItem('cart-products',JSON.stringify(products));
             display(products)
         })
         img.src = products[index].img;
@@ -31,26 +33,22 @@ function display(products){
         div.append(name,qty,price,btn);
         item.append(img,div);
         items.append(item);
-        document.getElementById('total-cost').textContent = total(pds)
+        document.getElementById('total-cost').textContent = total(products)
+        if(products.length==0)
+        document.getElementById('total-cost').textContent = 0
     })
-    if(products.length==0){
-        items.innerText="Nothing to see here"
-        document.getElementById('tp').style.display = 'none'
-    }
 }
-display(pds);
+display(products);
 //getting total price
 function total(arr){
-    let res = 0;
-    arr.map(el=>{
-res+=(parseInt(el.price) * parseInt(el.qty));
-    })
-    return res
+    return arr.reduce(function(acc,element,index){
+       return acc+ + +arr[index].price;
+    },0)
 }
 
 // console.log(total(products));
 //redirecting to address page
-let nxtPage = document.querySelector('#tp>button');
+var nxtPage = document.querySelector('#tp>button');
 // console.log(nxtPage)
 nxtPage.addEventListener('click',function(event){
     window.location.href='./address.html';
