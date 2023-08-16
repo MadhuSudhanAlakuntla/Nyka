f954fdd6fa7e275d058cddc109bd2b7c5b233c1f
KGS97 <work.dipanshu.singh@gmail.com>
2023-08-16 17:10:50 +0530

    reverted to original

--- cartAndPayment/cart.js
+++ cartAndPayment/cart.js
@@ -1,24 +1,49 @@
-
-let pds = JSON.parse(localStorage.getItem('cart-products'))||[];
-let items = document.getElementById('items')
+var products = [
+    {
+        img : 'https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/c/bc1607aPLUMX00000133_1507231.jpg',
+        name : 'Fair&lovely',
+        qty : 1,
+        price : 20000,
+    },
+    {
+        img : 'https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/c/bc1607aPLUMX00000133_1507231.jpg',
+        name : 'Fair&lovely',
+        qty : 2,
+        price : 20000,
+    },
+    {
+        img : 'https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/c/bc1607aPLUMX00000133_1507231.jpg',
+        name : 'Fair&lovely',
+        qty : 1,
+        price : 20000,
+    },
+    {
+        img : 'https://images-static.nykaa.com/media/catalog/product/tr:h-800,w-800,cm-pad_resize/b/c/bc1607aPLUMX00000133_1507231.jpg',
+        name : 'Fair&lovelydhjhgvksjfjkljfjks',
+        qty : 2,
+        price : 20000,
+    },
+    
+]
+// console.log(products);
+var items = document.getElementById('items')
 function display(products){
     items.innerHTML = "";
-    document.getElementById('tp').style.display = 'block'
     document.getElementById('total-items').innerText = products.length;
     products.forEach(function(element,index){
-        let item = document.createElement('div'); 
+        var item = document.createElement('div'); 
         item.className = 'item';
-        let div = document.createElement('div'); 
-        let img = document.createElement('img');
-        let name = document.createElement('p');
-        let qty = document.createElement('p');
-        let price = document.createElement('h6');
-        let btn = document.createElement('button');
+        var div = document.createElement('div'); 
+        var img = document.createElement('img');
+        var name = document.createElement('p');
+        var qty = document.createElement('p');
+        var price = document.createElement('h6');
+        var btn = document.createElement('button');
         btn.textContent = 'Remove';
         btn.addEventListener('click',function(event){
             event.preventDefault();
             products.splice(index,1);
-            localStorage.setItem('cart-products',JSON.stringify(products));
+            // localStorage.setItem('cart-products',JSON.stringify(products));
             display(products)
         })
         img.src = products[index].img;
@@ -31,26 +56,20 @@ function display(products){
         div.append(name,qty,price,btn);
         item.append(img,div);
         items.append(item);
-        document.getElementById('total-cost').textContent = total(pds)
+        document.getElementById('total-cost').textContent = total(products)
     })
-    if(products.length==0){
-        items.innerText="Nothing to see here"
-        document.getElementById('tp').style.display = 'none'
-    }
 }
-display(pds);
+display(products);
 //getting total price
 function total(arr){
-    let res = 0;
-    arr.map(el=>{
-res+=(parseInt(el.price) * parseInt(el.qty));
-    })
-    return res
+    return arr.reduce(function(acc,element,index){
+       return acc+ + +arr[index].price;
+    },0)
 }
 
 // console.log(total(products));
 //redirecting to address page
-let nxtPage = document.querySelector('#tp>button');
+var nxtPage = document.querySelector('#tp>button');
 // console.log(nxtPage)
 nxtPage.addEventListener('click',function(event){
     window.location.href='./address.html';
