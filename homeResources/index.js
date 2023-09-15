9b13ed8dc88b795f223911f0a05485cddf7296b4
sachin2398 <smartsachin38@gmail.com>
2023-09-15 19:51:51 +0530

    home-page index done

--- homeResources/index.js
+++ homeResources/index.js
@@ -3,11 +3,12 @@ let swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-  },
+  },  
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+    
   breakpoints: {
     640: {
       slidesPerView: 1,
